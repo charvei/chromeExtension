8eaa6cd873536a41446a4f788afbff81b883b44d
Craig Harvey <craigharvei@gmail.com>
2017-09-11 13:47:04 +1000

    updating for YT 2017, removing junk scripts & comments

--- eventpage.js
+++
@@ -1,6 +0,0 @@
-console.log("hello!!!!!!");
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-	console.log("elekrgjelkg");
-	chrome.tabs.executeScript("")
-});
--- script.js
+++ script.js
@@ -14,7 +14,7 @@
 /*Deal with multiple timestamps in the one comment: 1:30 for xxxx, 5:24 for xxxx*/
 
 
-document.addEventListener("spfdone", startRunning);
+document.addEventListener("yt-navigate-finish", startRunning);
 var key = "key=AIzaSyDrusteFhAYvTfF77mYNL-7MDmso7UyuVs";
 var part = "part=snippet,replies";
 var videoId;
@@ -32,13 +32,12 @@ startRunning();
 
 //Start interval timer and allow for timestamps to be display
 function startRunning() {
-	process();
 	videoId = getVideoId();
 	timeStampComments = new Array();
 	timeStampRecord = new Array();
 	fetchComments();
 	timer = checkVideoCurrentTimePeriodically();
-	document.addEventListener("spfrequest", function() {
+	document.addEventListener("yt-navigate-finish", function() {
 		clearInterval(timer);
 		clearTimestampMarkers();
 		timeStampComments = new Array();
@@ -53,13 +52,11 @@ function getVideoId() {
 	var videoId = "videoId=";
 	var endIndexOfVideoId;
 	var startIndexOfVideoId = (url.indexOf("?v=") + 3);
-//	var endIndexOfVideoId = indexOf("&", startIndexOfVideoId);
 	if ((endIndexOfVideoId = url.indexOf("&", startIndexOfVideoId))==-1) {
 		videoId += url.substr(startIndexOfVideoId, url.length);
 	} else {
 		videoId += url.substr(startIndexOfVideoId, endIndexOfVideoId);	
 	}
-	console.log(url);
 	return videoId;
 }
 
@@ -68,7 +65,6 @@ function fetchComments(nextPageToken) {
 	if (nextPageToken) {
 		urlRequest = APIurl + "?" + part + "&" + videoId + "&" + maxResults + "&pageToken=" + nextPageToken + "&" + key;
 	}
-	console.log(urlRequest);
 
 //fetch.then.then.catch skeleton borrowed from https://developers.google.com/web/ilt/pwa/working-with-the-fetch-api
 	fetch(urlRequest).then(function(response) {
@@ -79,20 +75,13 @@ function fetchComments(nextPageToken) {
 		return response.json();
 	}).then(function(responseAsJson) {
 		//Do stuff with the JSON
-		console.log(responseAsJson.items.length);
 		if (responseAsJson.nextPageToken) {
 			fetchComments(responseAsJson.nextPageToken);
-			console.log("next page comments have been fetched for");
 			parseResponseForStamps(responseAsJson);
-//			makeTimeStampElements(jarray);
 		} else {
 			//function has retrieved all comments
 			parseResponseForStamps(responseAsJson);
-//			makeTimeStampElements(jarray);
-			console.log("No next page");
-
 		}
-		console.log(responseAsJson);
 	}).catch(function(error) {
 		console.log('looks like there was a problem: ', error);
 	});
@@ -103,33 +92,19 @@ function parseResponseForStamps(response) {
 	var timeStamp;
 	for (i=0; i<response.items.length; i++) {
 		var text = response.items[i].snippet.topLevelComment.snippet.textOriginal;
-		console.log("text length : -> " + text.length);
 		//check if message text is too long
 		if (text.length > maxCommentLength) {
-			console.log("message is too long: +" + text.length + " characters.");
 			break;
 		}
 		//Pattern matching to find timestamp format
 		var regex = /\d\d:\d\d:\d\d|\d:\d\d:\d\d|\d\d:\d\d|\d:\d\d/;
 		if (timeStamp = text.match(regex)) {
-			console.log("timestamp found that isn't too long");
 			var author = response.items[i].snippet.topLevelComment.snippet.authorDisplayName;
 			var likes = response.items[i].snippet.topLevelComment.snippet.likeCount;
 			var profilePicUrl = response.items[i].snippet.topLevelComment.snippet.authorProfileImageUrl;
 			var publishTime = response.items[i].snippet.topLevelComment.snippet.publishedAt;
 			timeStampComments.push(new Array(author, text, likes, profilePicUrl, publishTime, timeStamp[0], convertStampToSeconds(timeStamp[0])));
 		}
-	}
-	console.log(timeStampComments);
-	//Is it better to add them all to one array, then parse the array or parse while in the json format.
-	//I guess the latter...
-}
-
-//Old function not in use (attempts goal of popTimeStampElements() below)
-function makeTimeStampElements() {
-	for (j=0; j<timeStampComments.length; j++) {
-		var id = "" + j;
-		makeDivForTimeStamp(id, timeStampComments[j][0], timeStampComments[j][1], timeStampComments[j][6], timeStampComments[j][3]);
 	}
 }
 
@@ -140,7 +115,6 @@ function popTimeStampElements() {
 		var timeStamp = timeStampComments.shift();
 		timeStamp.push(jarray);
 		timeStampRecord.push(timeStamp);
-		console.log(timeStampRecord);
 		makeDivForTimeStamp(id, timeStamp[0], timeStamp[1], timeStamp[6], timeStamp[3]);
 	}
 }
@@ -150,20 +124,12 @@ function makeDivForTimeStamp(timeStampId, author, text, timeStamp, displayPic) {
 	var videoDuration = $("#movie_player > div.html5-video-container > video").prop("duration");
 	var timeStampXPos = (timeStamp/videoDuration)*100;
 
-	console.log("TIMESTAMP AND VIDEODURATION: " + timeStamp + ", " + videoDuration);
-
 	//The Div containing message to be displayed @ timestamp time.
 	$('<div id=timeStamp' + timeStampId + ' class="timestampcomment" aria-live="assertive" aria-atomic="true" data-layer="4">' + '</div>').appendTo("div#movie_player");
-	console.log("display pic ->" + displayPic);
 	$('<div class="commentDisplayPic">' + '<img src=' + displayPic + '></div>').appendTo('div#timeStamp' + timeStampId + '');
 	$('<div class="commentMessage"></div>').appendTo('div#timeStamp' + timeStampId + '');
 	$('<div class="commentAuthor">' + author + ' said:</div>').appendTo('div#timeStamp' + timeStampId + '> div.commentMessage');
 	$('<div class="commentText">' + text + '</div>').appendTo('div#timeStamp' + timeStampId + '> div.commentMessage');
-
-
-	//This WORKS. Is the Div containing message to be displayed @ timestamp time.
-//	$('<div id=timeStamp' + timeStampId + ' class="ytp-paid-content-overlay timestampcomment" aria-live="assertive" aria-atomic="true" data-layer="4" style="display: none; background-color:rgba(0, 0, 0, 0.7); border-radius: 5px; padding-left: 5px; padding-right: 5px; transform: translate(-50%, 0); left : 50%; font-size: 16px; font-family: Arial; position: absolute; z-index: 24; height: auto;">' + author + ': ' + text + '</div>').appendTo("div#movie_player");
-
 	$('<div id=timeStampPosition' + timeStampId + ' class="ytp-ad-progress timestampmarker" style="left:' + timeStampXPos + '%; width: 6px; background: #0fb707;"></div>').appendTo("#movie_player > div.ytp-chrome-bottom > div.ytp-progress-bar-container > div.ytp-progress-bar > div.ytp-progress-list");
 }
 
@@ -175,13 +141,10 @@ function convertStampToSeconds(timeStamp){
 	if (timeStamp.length<6) {
 		//max digit represents minutes
 		timeSeconds = (+timeStampSplit[0])*60 + (+timeStampSplit[1]);
-//		console.log("Timestamp: " + timeStamp + ", converted to seconds -> " + timeSeconds);
 		return timeSeconds;
 	} else if (timeStamp.length<9) {
 		//max digit represents hours
 		timeSeconds = (+timeStampSplit[0])*60*60 + (+timeStampSplit[1])*60 + (+timeStampSplit[2]);
-//		console.log("timest[0], [1], [2] -->" + timeStampSplit[0]  + ", " + timeStampSplit[1] + ", " + timeStampSplit[]);
-//		console.log("Timestamp: " + timeStamp + ", converted to seconds -> " + timeSeconds);
 		return timeSeconds;
 	} else {
 		return false;
@@ -189,13 +152,11 @@ function convertStampToSeconds(timeStamp){
 }
 
 //Check if video has been loaded sufficiently on page to retrieve data from it
-function videoHasLoaded(){
+function hasVideoLoaded(){
 	if (($("#movie_player > div.html5-video-container > video").prop("readyState"))>1) {
 		//Video has loaded metadata
-		console.log("VIDEO HAS LOADED META DATA");
 		return 1;
 	} else {
-		console.log("VIDEO HAS NOTNOTNOTNOTNOTNOT loaded metadata");
 		return 0;
 	}
 }
@@ -217,41 +178,26 @@ function checkVideoCurrentTimePeriodically(){
 			var commentHeight;
 			var concurrentStamps = 0;
 			var durationSeconds = $("#movie_player > div.html5-video-container > video").prop("currentTime");
-			if (((timeStampComments.length)>0) && videoHasLoaded()) {
+			if (((timeStampComments.length)>0) && hasVideoLoaded()) {
 				popTimeStampElements();
-			} else {
-				console.log("failed if statement");
 			}
 			for (i=0; i<timeStampRecord.length; i++) {
-//				console.log(timeStampComments[i][6]);
 				if (compareStampToVideoTime(timeStampRecord[i][6], durationSeconds)) {
 					//timeStamp within range of current time
-					console.log("TIMESTAMP #" + i + " SHOULD BE SHOWN NOW, TIMESTAMP:" + timeStampRecord[i][6] + " , CURRENT VIDEO TIME:" + durationSeconds);
 					if (concurrentStamps<1) {
 						$('div#timeStamp' + i + '').addClass("timestamp-displaying");
 						$('div#timeStamp' + i + '').css('display', 'block');
-					//	if (($('#movie_player').hasClass("paused-mode")) || (($('#movie_player').hasClass("paused-mode")))
-					/*	if ($('#movie_player').hasClass("ytp-autohide")) {
-							$('div#timeStamp' + i + '').css('bottom', '10px');
-						} else {
-							//commentHeight = $('#movie_player > div.ytp-chrome-bottom').css('bottom') + 10;
-							$('div#timeStamp' + i + '').css('bottom', ('50px'));
-						}
-					*/
 						concurrentStamps++;
 					} else {
-//						$('div.timestamp-displaying').append('<div>Concurrent: ' + concurrentStamps + '</div>');
 						$('div#timeStamp' + i + '').css('display', 'none');
 						concurrentStamps++;
 					}	
-					console.log("CONCURRENT STAMPS: " + concurrentStamps);
 				} else {
 					//timeStamp not within range
 					$('div#timeStamp' + i + '').css('display', 'none');
 				}
 			}
 //			$('div.timestamp-displaying').append('<div>Concurrent: ' + concurrentStamps + '</div>');
-			console.log("Interval function run; currenttime:" + durationSeconds);
 		}, 500);
 	return timer;
 }
@@ -267,98 +213,14 @@ function compareStampToVideoTime(timeStampTime, durationSeconds) {
 		//Video is currently within the time of a stamp
 		return true;
 	} else {
-//		console.log("comparison false, stamp:" + timeStampTime + " , durationSeconds: " + durationSeconds);
 		return false;
 	}
-}
-
-function process() {
-	$('<button/>', {
-		id : 'timestamp-button',
-		class : 'yt-uix-button yt-uix-button-size-default yt-uix-button-opacity action-panel-trigger yt-uix-tooltip button-show-icon button-show-text',
-		type : 'button',
-		title : 'Show time stamps'
-		}).appendTo("#watch8-secondary-actions");
-
-	$('<span/>', {
-		id : 'timestamp-button-icon-wrapper',
-		class : 'yt-uix-button-icon-wrapper'
-	}).appendTo("#timestamp-button");
-
-	$('<span/>', {
-		id : 'timestamp-button-icon',
-		class : 'yt-uix-button-icon yt-sprite'
-	}).appendTo("#timestamp-button-icon-wrapper");
-
-	$('<label/>', {
-		class : 'enabled',
-		innerHTML : 'enabled'
-	}).appendTo("#timestamp-button-icon");
-
-	$('<label/>', {
-		class : 'disabled',
-		innerHTML : 'disabled'
-	}).appendTo("#timestamp-button-icon");
-
-
-	$('<span/>', {
-		id : 'timestamp-button-content',
-		class : 'yt-uix-button-content'
-	}).appendTo("#timestamp-button");
-
-	$("#timestamp-button-content").html("TimeStamps");
-
-	makeActionPanel();
-
-	$('button#timestamp-button').on('click', function() {
-		$('button#timestamp-button').toggleClass("timestamps-activated");
-		$('button#timestamp-button').toggleClass("yt-uix-button-toggled");
-		togglePanelDisplay();
-		var durationSeconds = $("#movie_player > div.html5-video-container > video").prop("currentTime");
-		console.log(durationSeconds);
-		console.log(true);
-		checkVideoCurrentTimePeriodically();
-	});
 }
 
 /*Clears the timestamp markers in progress bar (useful for spf loading of videos)*/
 function clearTimestampMarkers() {
 	$('.timestampmarker').remove();
 	$('.timestampcomment').remove();
-}
-
-function togglePanelDisplay() {
-	$('div#watch-action-panels').toggleClass("hid");
-
-	if ($('div#watch-action-panels').css('display') == 'none') {
-		$('div#watch-action-panels').css('display', 'block');
-	} else {
-		$('div#watch-action-panels').css('display', 'none');
-	}
-	
-	if ($('div#action-panel-timestamps').hasClass('hid')) {
-		//Hide all sub divs of div#watch-action-panels
-		$('div#watch-action-panels').children().addClass("hid");
-		$('div#watch-action-panels').children().css('display', 'none');
-
-		//Display timestamp panel
-		$('div#action-panel-timestamps').toggleClass("hid");
-		$('div#action-panel-timestamps').css('display', 'block');
-
-	} else {
-		$('div#action-panel-timestamps').toggleClass("hid");
-		$('div#action-panel-timestamps').css('display', 'none');
-	}
-}
-
-function makeActionPanel() {
-	$('div#watch-action-panels').css('display', 'none');
-	$('<div/>', {
-		id : 'action-panel-timestamps',
-		class : 'action-panel-content hid',
-		style : 'display : none'
-	}).appendTo('div#watch-action-panels');
-	$('div#action-panel-timestamps').append("<div>Heya!</div>");
 }
 
 
